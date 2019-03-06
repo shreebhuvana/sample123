6f75cceac06a8e4891f7e094424b92998e5021f1
shreebhuvana <shreebhuvana6@gmail.com>
2019-03-06 13:30:35 +0530

    changed

--- src/org/test/FIleOpertaion.java
+++ src/org/test/FIleOpertaion.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 public class FIleOpertaion {
 	public static void main(String[] args) throws IOException {
 		
-		int a= 10/0;
-		System.out.println(a);
+		System.out.println("hello");
 		
 		}	
 		}
